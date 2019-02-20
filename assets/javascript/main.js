51b4e71af768f8798b1bf256784a583251e99b8a
Veli Yagci <veliyagci35@gmail.com>
2019-02-19 19:33:34 -0500

    first commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,39 @@
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
+
+
+	if (question1 == "Abraham Lincoln") {
+		correct++;
+}
+	if (question2 == "Jeff") {
+		correct++;
+}	
+	if (question3 == "Baby") {
+		correct++;
+	}
+	
+    var messages = ["You rock!", "Eh...", "Never mind"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
